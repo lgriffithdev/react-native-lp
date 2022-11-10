ca09919606ba96737812788e9b7a25e68a40974e
Luciano Griffith <lgriffith.dev@gmail.com>
2022-11-10 14:15:46 +0100

    feat: init store, api call and test getting data

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import getApiContent from '@src/api';
+import { Text } from 'react-native';
+
+import { useValorantStore } from '@src/store';
+
 
 export default function App() {
+  const data = useValorantStore((state: any) => state.data)
+  const fetch = useValorantStore((state: any) => state.fetch)
+
+  useEffect(() => {
+    fetch()
+  }, [])
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Geoffrey mange tes morts !</Text>
+      {data &&
+        (
+          <Text>Data is loaded</Text>
+        )
+      }
+      {!data && (
+        <Text>Data is not loaded</Text>
+      )}
       <StatusBar style="auto" />
     </SafeAreaView>
   );
